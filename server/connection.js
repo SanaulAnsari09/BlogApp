f77dee12683bfd2f4feab3cb38d1fc91397dd1e2
SanaulAnsari09 <ansarcode09@gmail.com>
2025-04-10 21:21:09 +0530

    created structure of application on client and server

---
+++ server/connection.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectToDatabase = async (url) =>{
+    mongoose.connect(url).then((vlaue)=>{
+        console.log("Database connected successfully !")
+    }).catch((err)=>{
+        console.log("Database connection failed");
+    })
+};
+
+module.exports = {connectToDatabase};
